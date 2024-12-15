b5db404c603cf968dd84ed69ed28dadb21885da1
Andy Luue <155850018+andytluu@users.noreply.github.com>
2024-12-14 16:41:19 -0800

    Added a display function

--- tic.js
+++ tic.js
@@ -1,3 +1,13 @@
-function board(){
-    const gameBoard = [];
-}
+const displayBoard = document.querySelector("#game-board-container");
+const Gameboard = (function board(){
+    const gameBoard = ["","","","","","","","",""];
+    
+    const display = () => {
+        gameBoard.forEach((elem,index) =>{
+        displayBoard.innerHTML += `<div class="space" id="${index}">${elem}</div>`;
+    })
+};
+    return {display};
+})();
+
+Gameboard.display();

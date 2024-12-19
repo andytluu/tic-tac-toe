cdcd66052b0ec1e3526b5d51254e877800c1b635
Andy Luue <155850018+andytluu@users.noreply.github.com>
2024-12-19 12:29:15 -0800

    Added event to each box

--- tic.js
+++ tic.js
@@ -1,13 +1,24 @@
 const displayBoard = document.querySelector("#game-board-container");
 const Gameboard = (function (){
-    const gameBoard = ["","","","","","","","",""];
+    let gameBoard = ["","","","","","","","",""];
     
     const display = () => {
+        displayBoard.innerHTML ="";
         gameBoard.forEach((elem,index) =>{
         displayBoard.innerHTML += `<div class="space" id="${index}">${elem}</div>`;
-    })
-};
-    return {display};
+    });
+    const spaces = document.querySelectorAll(".space"); 
+    spaces.forEach((elem)=>{
+        elem.addEventListener("click",Game.handleClick);
+        });
+    };
+
+    const update = (index,mark) => {
+        gameBoard[index] = mark;
+        display();
+        console.log(gameBoard);
+    };
+    return {display,update};
 })();
 
 function createplayer(name,symbol){
@@ -19,7 +30,19 @@ const Game = (function (){
         createplayer("Player 1", "X"),
         createplayer("Player 2", "O")
     ];
+    let currentPlayer = 0;
+    const handleClick = (e) =>{
+        if(currentPlayer === 0){
+            Gameboard.update(parseInt(e.target.id),player[currentPlayer].symbol);
+            currentPlayer = 1;
+        }
+        else{
+            Gameboard.update(parseInt(e.target.id),player[currentPlayer].symbol);
+            currentPlayer = 0;
+        }
 
-    
+    };
+
+    return {handleClick};
 })();
 Gameboard.display();
